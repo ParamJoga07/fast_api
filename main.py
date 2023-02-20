725fceb46e5ef1911c53f8b1c794f64c74c3c3cc
PARMESHWAR JOGA <68342005+ParamJoga07@users.noreply.github.com>
2023-02-20 17:51:25 +0530

    ADDED functioinality to save file in firebase

--- main.py
+++ main.py
@@ -2,21 +2,24 @@ from fastapi import FastAPI, File, UploadFile, HTTPException
 from pydantic import BaseModel
 from google.cloud import storage
 import aiofiles
-import firebase_admin 
+import firebase_admin
 from firebase_admin import credentials
+import pyrebase
 
-cred = credentials.Certificate('/Users/apple/Downloads/xytech-test-firebase-adminsdk-9e882-c2dbc87811.json')
+
+cred = credentials.Certificate('/Users/apple/Downloads/xytech-test-firebase-adminsdk-9e882-31a2ff44b5.json')
 firebase_admin.initialize_app(cred)
 
+config = {
+  "apiKey": "AIzaSyBxJEPJNiStVHfO8stAUIpBvceFTVzESDA",
+  "authDomain": "xytech-test.firebaseapp.com",
+  "databaseURL": "https://xytech-test-default-rtdb.firebaseio.com",
+  "storageBucket": "xytech-test.appspot.com"
+}
+
+firebase = pyrebase.initialize_app(config)
+storage = firebase.storage()
 app = FastAPI()
-
-# define the request payload using pydantic BaseModel
-class FilePayload(BaseModel):
-    file: UploadFile
-
-# initialize the Firebase Storage client
-client = storage.Client.from_service_account_json('/Users/apple/Downloads/xytech-test-firebase-adminsdk-9e882-c2dbc87811.json')
-bucket = client.get_bucket('xytech-test.appspot.com')
 
 # define the FastAPI endpoint to upload a file
 @app.post('/upload')
@@ -27,16 +30,12 @@ async def upload_file(payload: UploadFile):
     if file_extension not in allowed_extensions:
         raise HTTPException(status_code=400, detail="File type not supported")
 
-    with open(payload.filename, "wb") as f:
-        contents = await payload.read()
-        f.write(contents)
+    storage.child("uploads/" + payload.filename).put(payload.file)
+
     return {"filename": payload.filename}
+    
 
-    # save the file to Firebase Storage
-    # blob = bucket.blob(payload.file.filename)
-    # blob.upload_from_string(await payload.file.read(), content_type=payload.file.content_type)
-
-    # return a success message
-    return {"message": "File uploaded successfully"}
+    
+    
 
 
